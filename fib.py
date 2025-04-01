06511d7d50a1f470ad709f0dff9af3b844f67723
Aditya Mewada <you@example.com>
2025-04-01 17:09:52 +0530

    UPDATE

--- fib.py
+++ fib.py
@@ -1,3 +1,5 @@
+# top - down 
+
 n = 5
 dp = [-1] * (n+1)
 
@@ -13,4 +15,15 @@ def fib(n,dp):
     dp[n] = fib(n-1,dp) + fib(n-2, dp)
     return dp[n]
 
-print("fib : ", fib(n,dp))
+print("fib :",fib(n,dp))
+
+# bottom up 
+
+f = [0] * (n+1)
+dp[0] = 0
+dp[1] = 1
+
+for i in range(2,n+1):
+    f[i] = f[i-1] + f[i-2]
+
+print("fib :",dp[n])    
